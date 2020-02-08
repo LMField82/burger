53e8ae7c8a099814fa0cc4d618015adadcd0e0e2
LindsayMF <meneslindsay@hotmail.com>
2020-02-07 18:17:34 -0700

    completed database schema and seed files

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Black and Blue Burger", false), ("Spicy Chicken Burger", false), 
+("Double Double Burger", false), ("Bacon Cheese Burger", false);

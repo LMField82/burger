0bfcf096245921536e4d8e6db723a20698266331
LindsayMF <meneslindsay@hotmail.com>
2020-02-18 18:43:16 -0700

    all buttons working

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,27 +3,6 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
-// router.get("/", function(req, res) {
-//     burger.selectAll(function(data) {
-//         res.render("index", { burgers: data });
-//     });
-// });
-
-// router.post("/api/burgers", function(req, res) {
-//     burger.insertOne(req.body.burger_name, function(data) {
-//         res.json({ id: res.insertId });
-//     });
-// });
-
-// router.put("/api/burgers/:id", function(req, res) {
-//     burger.updateOne(req.params.id, function(data) {
-//         if (data.changeedRows == 0) {
-//             return res.status(404);
-//         }
-//     });
-// });
-
-
 router.get("/", function(req, res) {
     res.redirect("/burgers");
 });
@@ -34,21 +13,6 @@ router.get("/burgers", function(req, res) {
     });
 });
 
-// router.post("/api/burgers", function(req, res) {
-//     burger.insertOne([
-//         "burger_name"
-//     ], 
-//     [
-//         req.body.burger_name
-//     ], 
-//         function(result) {
-//             console.log(result)
-//             res.json({ id: result.insertId });
-//     });
-// });
-
-
-
 router.post("/api/burgers", function(req, res) {
     // takes the request object using it as input for burger.addBurger
     burger.insertOne(req.body.burger_name, function(result) {
@@ -58,18 +22,6 @@ router.post("/api/burgers", function(req, res) {
       res.redirect("/");
     });
 });  
-
-// router.put("/burgers/:id", function(req, res) {
-//     let condition = "id = " + req.params.id;
-    
-//     burger.updateOne({
-//         devoured: true
-//     }, condition, function(data) {
-//         res.redirect("/");
-//     });
-   
-//     console.log("condition", condition)
-// });
 
 router.put("/api/burgers/:id", function(req, res) {
     burger.updateOne(req.params.id, function(result) {

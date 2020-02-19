c3c0b667791b3a2ca6102f73631f38b8eb3bcf21
LindsayMF <meneslindsay@hotmail.com>
2020-02-19 13:54:52 -0700

    styling in progress

--- config/orm.js
+++ config/orm.js
@@ -77,27 +77,4 @@ let orm = {
     }
 }
 
-
-
-    // selectAll: function(cb) {
-    //     connection.query(`SELECT * FROM burgers`, function(err, data) {
-    //         if (err) throw err;
-    //         cb(data);
-    //     });
-    // },
-    // insertOne: function(newBurger, cb) {
-    //     connection.query(`INSERT INTO burgers (burger_name) VALUES ('${newBurger}')`, function(err, data) {
-    //         if (err) throw err;
-    //         cb(data);
-    //     });
-    // },
-    // updateOne: function(id, cb) {
-    //     connection.query(`UPDATE burgers SET devoured = TRUE WHERE id = ${id}`, function(err, res) {
-    //         if (err) throw err;
-    //         cb(res);
-    //     });
-    // }
-
-
-
 module.exports = orm;
